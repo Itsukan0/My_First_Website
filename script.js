5b8f281a702157913fb3de8c6a221a6ca96a4f27
Itsuka0 <hassimmazahere@gmail.com>
2023-05-25 18:53:39 +0200

    feat (exercice 1): working example

--- script.js
+++ script.js
@@ -1,45 +1,71 @@
 console.log("Shapes and Loops Execercise")
 
-DrawLine = function(){
-    var canvas = document.getElementById('line');
+DrawShape = function(pointsArray, canvasID, fill = false){
+    console.log("Drawing a line !")
+    if (!pointsArray && !extCanvas_id){
+        console.error("Args are invalid !")
+        return false
+    }
+
+    id = canvasID
+    var canvas = document.getElementById(id);
+
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
         // code de dessin dans le canvas
         if (ctx) {
-            console.log("Got the context !")
+            let i = 0;
+            beginPoint = pointsArray[0]
+
+            ctx.beginPath();
+            ctx.moveTo(beginPoint[0], beginPoint[1]);
+            while (i < (pointsArray.length-1)){
+                beginPoint = pointsArray[i];
+                stopPoint = pointsArray[i+1];
+                ctx.lineTo(stopPoint[0], stopPoint[1]);
+                console.log("Drew Line from : (" + beginPoint.join() + ") to ("+  stopPoint.join()+")")
+                i++;
+            }
+            if (fill){
+                console.log("We fill the Path !")
+                ctx.fill();
+            }
+            ctx.closePath();
+            ctx.stroke();
+            return true
         }
         else{
             console.log("Error : coudn't fetch canvas 2d context")
         }
       }
+    return false
 }
 
-DrawSquare = function (){
-    var canvas = document.getElementById('square');
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-        // code de dessin dans le canvas
-        console.log("Got the context !")
-      }
-      else{
-        console.log("Error : coudn't fetch canvas 2d context")
-    }
-}
-
-DrawTriangle = function (){
-    var canvas = document.getElementById('triangle');
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-        // code de dessin dans le canvas
-        console.log("Got the context !")
-    }
-    else{
-        console.log("Error : coudn't fetch canvas 2d context")
-    }
-}
 
 Draw = function(){
-    DrawLine();
-    DrawSquare();
-    DrawTriangle();
+    // Line
+    if (!DrawShape([[50,50],[100,100]],'line')){
+        console.error("Coudn't Draw Line !")
+        return
+    }
+    else{
+        console.log("Sucessfull Drawing of Line !")
+    }
+    // Square
+    // We could also use ctx.rect but I suppose it's not what is asked here, too simple
+    if (!DrawShape([[10,10], [10,100], [100,100], [100,10], [10,10]],'square', true)){
+        console.error("Coudn't Draw Square !")
+        return
+    }
+    else{
+        console.log("Sucessfull Drawing of Square !")
+    }
+    if (!DrawShape([[10,10], [10,100], [100,100], [10,10]],'triangle', false)){
+        console.error("Coudn't Draw Triangle !")
+        return
+    }
+    else{
+        console.log("Sucessfull Drawing of Triangle !")
+    }
+    return
 }
