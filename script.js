69015941690cdf00f22f048f089e92b826b71c55
Itsukan0 <hassimmazahere@gmail.com>
2025-09-07 23:41:20 +0200

    feat (js): apple in the canvas

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.onload = function () {
     var ctx;
     var delay = 100;
     var snakey;
+    var applee;
 
     init();
 
@@ -17,6 +18,7 @@ window.onload = function () {
         document.body.appendChild(canvas)
         ctx = canvas.getContext('2d');
         snakey = new Snake([[6, 4], [5, 4], [4, 4]], "right")
+        applee = new Apple([10, 10]);
         refreshCanvas();
     };
 
@@ -24,6 +26,7 @@ window.onload = function () {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         snakey.advance();
         snakey.draw();
+        applee.draw();
         setTimeout(refreshCanvas, delay);
     };
 
@@ -86,6 +89,21 @@ window.onload = function () {
             }
         };
     };
+    
+    function Apple(position) {
+        this.position = position;
+        this.draw = function(){
+            ctx.save();
+            ctx.fillStyle = "#33cc33";
+            ctx.beginPath();
+            var radius = blockSize/2;
+            var x = position[0]*blockSize + radius;
+            var y = position[1]*blockSize + radius;
+            ctx.arc(x, y, radius, 0, Math.PI*2, true);
+            ctx.fill()
+            ctx.restore();
+        }
+    }
 
     document.onkeydown = function handleKeydown(e) {
         var key = e.keyCode;
