1444ac30f9f879020c6a2d1c68fecea144c56e8f
Itsukan0 <hassimmazahere@gmail.com>
2025-09-08 00:00:04 +0200

    feat (js): add collision system

--- script.js
+++ script.js
@@ -19,15 +19,22 @@ window.onload = function () {
         ctx = canvas.getContext('2d');
         snakey = new Snake([[6, 4], [5, 4], [4, 4]], "right")
         applee = new Apple([10, 10]);
+        widthInBlocks = canvasWidth/blockSize;
+        heightInBlocks = canvasHeight/blockSize;
         refreshCanvas();
     };
 
     function refreshCanvas() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
         snakey.advance();
-        snakey.draw();
-        applee.draw();
-        setTimeout(refreshCanvas, delay);
+        if(snakey.checkCollision()){
+
+        }
+        else{
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            snakey.draw();
+            applee.draw();
+            setTimeout(refreshCanvas, delay);
+        }
     };
 
     function drawBlock(ctx, position) {
@@ -88,6 +95,34 @@ window.onload = function () {
                 this.direction = newDirection;
             }
         };
+
+        this.checkCollision = function(){
+            var wallCollision = false;
+            var snakeCollision = false;
+            var head = this.body[0];
+            var rest = this.body.slice(1);
+            var snakeX = head[0];
+            var snakeY = head[1];
+            var minX = 0;
+            var minY = 0;
+            var maxX = widthInBlocks - 1;
+            var maxY = heightInBlocks - 1;
+
+            var isNotBetweenHorizontalWalls = snakeX < minX || snakeX > maxX;
+            var isNotBetweenVerticalWalls = snakeY < minY || snakeY > maxY;
+
+            if(isNotBetweenHorizontalWalls || isNotBetweenVerticalWalls){
+                wallCollision = true;
+            }
+
+            for(var i = 0; i< rest.length; i++){
+                if(snakeX===rest[i][0] && snakeY===rest[i][1]){
+                    snakeCollision = true;
+                }
+            }
+
+            return wallCollision || snakeCollision;
+        }
     };
     
     function Apple(position) {
